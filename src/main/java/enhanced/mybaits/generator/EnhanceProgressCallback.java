f00274eba620436fbbfc7b8c9db6b9406a1afb30
xuml <xuml@abcintel.cn>
2023-09-07 10:07:21 +0800

    fix: 修改处理回调的输出方式

--- src/main/java/enhanced/mybaits/generator/EnhanceProgressCallback.java
+++ src/main/java/enhanced/mybaits/generator/EnhanceProgressCallback.java
@@ -3,43 +3,40 @@ package enhanced.mybaits.generator;
 
 import org.mybatis.generator.internal.NullProgressCallback;
 
-import lombok.extern.log4j.Log4j2;
-
 /**
  * MyBaits Generatior 增强处理进度回调处理
  * @author 徐明龙 XuMingLong 
  */
-@Log4j2
 public class EnhanceProgressCallback extends NullProgressCallback {
 
 
     @Override
     public void generationStarted(int totalTasks) {
-        log.info("生成代码任务开始");
+        System.out.println("生成代码任务开始,待处理的任务数" + totalTasks);
     }
 
 
     @Override
     public void introspectionStarted(int totalTasks) {
-        log.info("解析表数据任务开始，任务数量:{}",totalTasks);
+        System.out.println("解析表数据任务开始,待处理的任务数" + totalTasks);
     }
 
 
     @Override
     public void saveStarted(int totalTasks) {
-        log.info("保存为文件任务开始，任务数量:{}",totalTasks);
+        System.out.println("保存为文件任务开始,待处理的任务数" + totalTasks);
     }
 
 
     @Override
     public void startTask(String taskName) {
-        log.info("开始执行任务:{}",taskName);
+        System.out.println("开始执行任务：" + taskName);
     }
 
 
     @Override
     public void done() {
-        log.info("所有任务完成");
+        System.out.println("所有任务完成");
     }
 
     
